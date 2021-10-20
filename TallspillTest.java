b834f09af48af52c1598cfbbd015e5fd7e619b38
RichJo02 <s362063@oslomet.no>
2021-10-20 14:32:52 +0200

    Test

---
+++ TallspillTest.java
@@ -0,0 +1,10 @@
+package oblig2;
+
+public class TallspillTest {
+    public static void main(String[] args) {
+        Tallspill mittspill = new Tallspill();
+
+
+    }
+
+}
---
+++ Test.java
@@ -0,0 +1,10 @@
+package oblig2;
+
+public class Test {
+    public static void main(String[] args) {
+        Tallspill tas = new Tallspill();
+        System.out.println(tas);
+
+
+    }
+}
